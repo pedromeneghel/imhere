3db94a191e70a79ef4fcda8aa24f8d7f32afbfad
Pedro Meneghel <pedromeneghel@gmail.com>
2023-07-19 10:36:36 -0300

    feat: adiciona listagem de participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,53 @@
-import { Text, TextInput, View } from 'react-native';
+import { FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
+  const participants = ['Pietra', 'Valentina', 'Elisete', 'Pedro', 'Sueli', 'Guilherme', 'Roseli', 'Roseneide', 'Eder', 'Zilda', 'Kauã', 'Kaio'];
+
+  function handleParticipantAdd() {
+    console.log('Adicionou participante');
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Remove participante ${name}`);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
+
       <Text style={styles.date}>Sexta, 5 de Novembro de 2023.</Text>
-      <TextInput 
-        style={styles.input} 
-        placeholder='Nome do participante'
-        placeholderTextColor="#6b6b6b"
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do participante'
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList 
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Participant 
+              key={item} 
+              name={item} 
+              onRemove={() => handleParticipantRemove(item)} 
+            />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.</Text>
+        )}
       />
     </View>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -22,6 +22,30 @@ export const styles = StyleSheet.create({
     borderRadius: 5,
     color: '#FDFCFE',
     padding: 16,
-    fontSize: 16
+    fontSize: 16,
+    flex: 1,
+    marginRight: 7
+  },
+  buttonText: {
+    color: '#FDFCFE',
+    fontSize: 24
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31cf67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42
+  },
+  listEmptyText: {
+    color: '#fff',
+    textAlign: 'center'
   }
 });
